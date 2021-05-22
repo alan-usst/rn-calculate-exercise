d179c13b19bf1cd549e8270fbe9a74405cef045e
lin.chen <lin.chen@yitu-inc.com>
2021-05-22 22:39:51 +0800

    连接字体库

--- ui-src/qs/QuickStart.js
+++ ui-src/qs/QuickStart.js
@@ -2,18 +2,13 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import {Button, Provider, Toast } from '@ant-design/react-native';
 
-import {QuickStart} from './ui-src';
-
-export default class App extends Component {
+export default class QuickStart extends Component {
   render() {
     return(
-      <Provider>
-        <QuickStart/>
-
+      
         <Button onPress={() => Toast.info('This is a toast tips')}>
-          Start
+          QuickStart
         </Button>
-      </Provider>
     )
   }
 }
