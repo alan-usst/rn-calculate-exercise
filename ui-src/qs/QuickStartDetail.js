40b7fe8d6fc53be5b28a1a9e97ada675fe493a1d
lin.chen <lin.chen@yitu-inc.com>
2021-05-24 12:25:21 +0800

    快速开始详情页,引入题目列表前的还原点

--- ui-src/qs/QuickStartDetail.js
+++ ui-src/qs/QuickStartDetail.js
@@ -1,22 +1,85 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, StyleSheet, TextInput } from 'react-native';
 import { Button, Toast, Flex, WingBlank, Checkbox, Icon } from '@ant-design/react-native';
-
-const CheckboxItem = Checkbox.CheckboxItem;
+import { QuickStartAPI } from '@api';
 
 export default class QuickStartDetail extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-      recordId: props.route.params.recordId
-  };
-}
+      recordId: props.route.params.recordId,
+      itemIndex: 1,
+      itemDetail: null,
+      recordDetail: null,
+      statisticsInfo: null,
+    };
+  }
+
+  getItemDetail = (recordDetail) => {
+    console.log("this.state.itemIndex", this.state.itemIndex);
+    let tmp = recordDetail.items.filter((element, index, array) => {
+      return element.index == this.state.itemIndex
+    });
+    return tmp.length == 0 ? null : tmp[0];
+  }
+
+  refreshState = (recordDetailStr) => {
+    let recordDetail = JSON.parse(recordDetailStr);
+    let itemDetail = this.getItemDetail(recordDetail);
+    this.setState({ itemDetail: itemDetail, recordDetail, recordDetail, statisticsInfo: recordDetail.statisticsInfo });
+  }
+
+  componentDidMount = () => {
+    const { refreshState } = this;
+    QuickStartAPI.getRecordDetailById(this.state.recordId, function (args) {
+      refreshState(args);
+    });
+  }
   render() {
     return (
-      <Button onPress={() => Toast.info('This is a toast tips')}>
-                {this.state.recordId}
-              </Button>
-      
+      <ScrollView
+        style={{ flex: 1 }}
+        automaticallyAdjustContentInsets={false}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}>
+        <WingBlank style={{ marginBottom: 0, marginTop: 0 }}>
+          <Flex direction="row" style={{paddingTop:20}}>
+            <Flex.Item style={{ paddingLeft: 4 }}>
+              <Text style={{ color: '#000', fontSize: 15, fontWeight: 'bold' }}>总题数：{this.state.statisticsInfo == null ? 0 : this.state.statisticsInfo.total}</Text>
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 15 }}>
+              <Text style={{ color: '#00ce00', fontSize: 15, fontWeight: 'bold' }}>答对：{this.state.statisticsInfo == null ? 0 : this.state.statisticsInfo.rightCount}</Text>
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 4 }}>
+              <Text style={{ color: '#fa2e3e', fontSize: 15, fontWeight: 'bold' }}>答错：{this.state.statisticsInfo == null ? 0 : this.state.statisticsInfo.wrongCount}</Text>
+            </Flex.Item>
+            <Flex.Item style={{ paddingLeft: 4, paddingRight: 4 }}>
+              <Text style={{ color: '#9f9898', fontSize: 15, fontWeight: 'bold' }}>未开始：{this.state.statisticsInfo == null ? 0 : this.state.statisticsInfo.undoCount}</Text>
+            </Flex.Item>
+          </Flex>
+
+          <Flex direction="row" style={{ paddingTop: 30 }} justify='around' >
+            <Flex.Item >
+              <Text style={{ color: '#000', fontSize: 23, fontWeight: 'bold', textAlign: 'center' }}>当前第 {this.state.itemIndex} 题</Text>
+            </Flex.Item>
+            <Flex.Item style={{}}>
+              <Button onPress={() => {
+                Toast.info('This is a toast tips');
+                console.log("查看题目列表")
+              }}
+                type='ghost' size='large' style={{maxWidth:150,
+                  minWidth: 100,
+                  wordWrap: 'break-word'}}>查看题目列表</Button>
+            </Flex.Item>
+          </Flex>
+
+          {/* <Button onPress={() => Toast.info('This is a toast tips')}>
+            {this.state.recordId}
+          </Button> */}
+        </WingBlank>
+      </ScrollView>
+
+
 
     )
   }
@@ -24,5 +87,5 @@ export default class QuickStartDetail extends Component {
 
 
 const styles = StyleSheet.create({
-  
+
 });
